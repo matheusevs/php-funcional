0cfbd6aeea236363dc9f29d47e8b18130fb9dd19
Matheus Eduardo <88885147+matheusevs@users.noreply.github.com>
2024-09-24 12:55:28 -0300

    Contando medalhas

--- manipulacao.php
+++ manipulacao.php
@@ -6,6 +6,14 @@ $contador = count($dados);
 
 echo $contador;
 
+function somarMedalhas($medalhasAcumuladas, $medalha) {
+    return $medalhasAcumuladas + $medalha;
+}
+
+function medalhasAcumuladas($medalhasAcumuladas, $pais) {
+    return $medalhasAcumuladas + array_reduce($pais['medalhas'], 'somarMedalhas', 0);
+}
+
 function converterPaisParaLetrasMaiusculas ($pais) {
     $pais['pais'] = mb_convert_case(($pais['pais']), MB_CASE_UPPER);
     return $pais;
@@ -18,4 +26,7 @@ function verificarSePaisTemEspacoNoNome($pais)
 
 $dados = array_map('converterPaisParaLetrasMaiusculas', $dados);
 $dados = array_filter($dados, 'verificarSePaisTemEspacoNoNome');
+
 var_dump($dados);
+
+echo array_reduce($dados, 'medalhasAcumuladas', 0);
