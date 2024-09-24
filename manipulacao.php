3503c32c2829af4c2f68a4dc18fb54f771c824eb
Matheus Eduardo <88885147+matheusevs@users.noreply.github.com>
2024-09-24 12:42:22 -0300

    Mapeando dados

--- manipulacao.php
+++ manipulacao.php
@@ -5,3 +5,11 @@ $dados = require 'dados.php';
 $contador = count($dados);
 
 echo $contador;
+
+function converterPaisParaLetrasMaiusculas ($pais) {
+    $pais['pais'] = mb_convert_case(($pais['pais']), MB_CASE_UPPER);
+    return $pais;
+}
+
+$dados = array_map('converterPaisParaLetrasMaiusculas', $dados);
+var_dump($dados);
