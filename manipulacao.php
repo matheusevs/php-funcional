82e65059b467088ac9a260a0f77ce1fd936e7638
Matheus Eduardo <88885147+matheusevs@users.noreply.github.com>
2024-09-25 11:43:34 -0300

    Simplificando o código

--- manipulacao.php
+++ manipulacao.php
@@ -6,46 +6,31 @@ $contador = count($dados);
 
 echo $contador;
 
-function somarMedalhas($medalhasAcumuladas, $medalha) {
-    return $medalhasAcumuladas + $medalha;
-}
-
-function medalhasAcumuladas($medalhasAcumuladas, $pais) {
-    return $medalhasAcumuladas + array_reduce($pais['medalhas'], 'somarMedalhas', 0);
-}
+$somarMedalhas = fn ($medalhasAcumuladas, $medalha) => $medalhasAcumuladas + $medalha;
 
 function converterPaisParaLetrasMaiusculas ($pais) {
     $pais['pais'] = mb_convert_case(($pais['pais']), MB_CASE_UPPER);
     return $pais;
 }
 
-function verificarSePaisTemEspacoNoNome($pais)
-{
-    return strpos($pais['pais'], ' ') !== false;
-}
+$verificarSePaisTemEspacoNoNome = fn ($pais) => strpos($pais['pais'], ' ') !== false;
 
-function compararMedalhas($medalhasPais1, $medalhasPais2)
-{
-    return function($modalidade) use ($medalhasPais1, $medalhasPais2) {
-        return $medalhasPais2[$modalidade] <=> $medalhasPais1[$modalidade];
-    };
-}
+$compararMedalhas = fn ($medalhasPais1, $medalhasPais2) => fn ($modalidade) => $medalhasPais2[$modalidade] <=> $medalhasPais1[$modalidade];
 
 $dados = array_map('converterPaisParaLetrasMaiusculas', $dados);
-$dados = array_filter($dados, 'verificarSePaisTemEspacoNoNome');
+$dados = array_filter($dados, $verificarSePaisTemEspacoNoNome);
 
 $medalhas = array_reduce(
-    array_map(function ($medalhas) {
-        return array_reduce($medalhas, 'somarMedalhas', 0);
-    }, array_column($dados, 'medalhas')),
-    'somarMedalhas',
+    array_map(fn ($medalhas) => array_reduce($medalhas, $somarMedalhas, 0),
+    array_column($dados, 'medalhas')),
+    $somarMedalhas,
     0
 );
 
-usort($dados, function ($pais1, $pais2) {
+usort($dados, function ($pais1, $pais2) use ($compararMedalhas) {
     $medalhasPais1 = $pais1['medalhas'];
     $medalhasPais2 = $pais2['medalhas'];
-    $comparador = compararMedalhas($medalhasPais1, $medalhasPais2);
+    $comparador = $compararMedalhas($medalhasPais1, $medalhasPais2);
 
     return $comparador('ouro') !== 0 ? $comparador('ouro') 
         : ($comparador('prata') !== 0 ? $comparador('prata') 
